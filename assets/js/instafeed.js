de383b7b043b98a6df850b5a3ad5b259ebbce704
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2022-07-26 19:35:53 +0530

    Add files via upload

--- assets/js/instafeed.js
+++ assets/js/instafeed.js
@@ -1,10 +1,11 @@
-    // Instafeed.js
-    var userFeed = new Instafeed({
-        get: "user",
-        target: "instafeed-container",
-        resolution: "low_resolution",
-        // template: '<a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a>',
-        accessToken:
-          "IGQVJXMW43QnhIN05jWGRMOUVVUHp6Y3g3TThqc0puLV9Ba0JRT24yS3RsOWJPdFZA3SF8wX0NYUERqUFVTQkUxUjRobkxxUzRSMlhWaUhzRDd5SS1DeTBacERzQjJ4X0ZAieWNIS3FXNzZAnOGZATOW1lVwZDZD",
-      });
-      userFeed.run();
+// Instafeed.js
+var userFeed = new Instafeed({
+  get: "user",
+    // target: "instafeed",
+  target: document.getElementById("instafeed"),
+  resolution: "low_resolution",
+  // template: '<a href="{{link}}"> <img title="{{caption}}" src="{{image}}" /></a>"',
+  accessToken:
+    "IGQVJXMW43QnhIN05jWGRMOUVVUHp6Y3g3TThqc0puLV9Ba0JRT24yS3RsOWJPdFZA3SF8wX0NYUERqUFVTQkUxUjRobkxxUzRSMlhWaUhzRDd5SS1DeTBacERzQjJ4X0ZAieWNIS3FXNzZAnOGZATOW1lVwZDZD",
+});
+userFeed.run();

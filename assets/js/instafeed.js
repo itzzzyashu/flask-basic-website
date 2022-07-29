d19d3a09a10e009661c47d26947edfe62889549e
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2022-07-29 15:47:13 +0530

    Fixed InstaFeed

--- assets/js/instafeed.js
+++ assets/js/instafeed.js
@@ -1,10 +1,10 @@
 // Instafeed.js
 var userFeed = new Instafeed({
   get: "user",
-    // target: "instafeed",
-  target: document.getElementById("instafeed"),
+  target: "instafeed",
+  // target: document.getElementById("instafeed"),
   resolution: "low_resolution",
-  // template: '<a href="{{link}}"> <img title="{{caption}}" src="{{image}}" /></a>"',
+  template: '<a href="{{link}}"> <img title="{{caption}}" src="{{image}}" /></a>',
   accessToken:
     "IGQVJXMW43QnhIN05jWGRMOUVVUHp6Y3g3TThqc0puLV9Ba0JRT24yS3RsOWJPdFZA3SF8wX0NYUERqUFVTQkUxUjRobkxxUzRSMlhWaUhzRDd5SS1DeTBacERzQjJ4X0ZAieWNIS3FXNzZAnOGZATOW1lVwZDZD",
 });

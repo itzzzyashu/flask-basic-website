ab1fb42268ad83f40e702647b35c7cb8fbf09e18
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2022-07-30 06:18:30 +0530

    Ok

--- assets/js/instafeed.js
+++ assets/js/instafeed.js
@@ -1,10 +1,1 @@
-// Instafeed.js
-var userFeed = new Instafeed({
-  get: "user",
-  target: "instafeed",
-  limit: 14,
-  resolution: "low_resolution",
-  template: '<a href="{{link}}"> <img title="{{caption}}" src="{{image}}" /></a>',
-  accessToken: "IGQVJVV2tJOFlhQkc2MFoxU2hjTUMtRk9ZATGtuakJIYWt5ZA2lsWDlKdkRiSkpZAb2JZAal9FUnhBcWU5aG0tdmxZAYTlaZAXpXQ3U2WkR5bkIwUUVvNUpGTFM1X2xzU2VjWjBCODl4ZAkdkWWQ5b3ZACcUxwbQZDZD",
-});
-userFeed.run();
+
